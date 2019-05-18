eedc20af709aeddd11fd52dcead9360f4645d332
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-05-18 14:01:43 -0600

    Formated documentation

--- src/index.js
+++ src/index.js
@@ -6,31 +6,23 @@ import * as serviceWorker from './serviceWorker';
 
 
 /** 
- * 1.Dependencies
-    * npm i redux
-*/
+  1.Dependencies
+     npm i redux
 
+  2.Creating a store
+    Takes Two Arguments
+     1. Argument is the reducer you want to use,
+     2. Argument is the state.
 
-/** 
- * 2.Creating a store
- * Takes Two Arguments
-    * 1. Argument is the reducer you want to use,
-    * 2. Argument is the state.
-*/
+  3.Creating a reducer
+    Takes Two Arguments
+     1. Argument is the state.
+     2. Argument is the action.
 
-
-/** 
- * 3.Creating a reducer
- * Takes Two Arguments
-    * 1. Argument is the state.
-    * 2. Argument is the action.
-*/
-
-/** 
- * 4.Creating a action
- * Takes Two Arguments
-    * 1. Argument is the type. ex: type:'ADD'
-    * 2. Argument is the payload. ex: payload: '1'
+  4.Creating a action
+    Takes Two Arguments
+     1. Argument is the type. ex: type:'ADD'
+     2. Argument is the payload. ex: payload: '1'
 */
 
 // Basic store
